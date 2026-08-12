ef4a46db29be12b88eac4ddc412d15a11f50d18e
mallium <mallium@gmail.com>
2026-08-12 14:05:03 -0400

    filtro onlySurvived para mostrar useState

--- app/page.js
+++ app/page.js
@@ -1,19 +1,34 @@
+"use client";
 import titanic from "./data/titanic.json";
 import Pasajero from "./components/pasajero";
+import { useState } from "react";
+
 export default function Home() {
   let nSurvived = 0;
   titanic.map((e) => {
     if (e.Survived == 1) nSurvived++;
   });
 
+  const [onlySurvived, setOnlySurvived] = useState(false);
+
+  const handleClick = () => {
+    setOnlySurvived(!onlySurvived);
+  };
+
   return (
     <div>
       <h1>Pasajeros del Titanic</h1>
       <div>Sobrevivieron {nSurvived}</div>
       <div>
-        {titanic.map((e, i) => (
-          <Pasajero key={i} indice={i} pData={e} />
-        ))}
+        <button onClick={handleClick}>ver sólo los sobrevivientes</button>
+      </div>
+      <div>
+        {titanic.map(
+          (e, i) =>
+            (!onlySurvived || e.Survived == 1) && (
+              <Pasajero key={i} indice={i} pData={e} />
+            ),
+        )}
       </div>
     </div>
   );
